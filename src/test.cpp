e2fb4fab007364d4c49f0b62eb6c6db1556ea02d
tpaau-17DB <mikolaj_archusr@tutamail.com>
2024-10-11 22:08:04 +0200

    Init

---
+++ src/test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "Logger.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"----------test start----------"<<endl<<endl;
+
+    cout<<"testing log filtering"<<endl;
+    
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<endl<<"Verbosity set to "<<i<<endl;
+        Logger::SetVerbosity(Logger::LogLevel(i));
+        Logger::PrintDebug("debug");
+        Logger::PrintLog("info");
+        Logger::PrintWarn("warning");
+        Logger::PrintErr("error");
+    }
+
+    cout<<endl<<"testing again for overrideFiltering set to true"<<endl;
+    Logger::SetOverrideFiltering(true);
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<endl<<"Verbosity set to "<<i<<endl;
+        Logger::SetVerbosity(Logger::LogLevel(i));
+        Logger::PrintDebug("debug");
+        Logger::PrintLog("info");
+        Logger::PrintWarn("warning");
+        Logger::PrintErr("error");
+    }
+    cout<<endl<<"----------test end----------"<<endl;
+    return 0;
+}
